b7ccf1f52112017853faf82774193d38ff1dae4c
Benjamin Sommervoll <puixdk@gmail.com>
2025-07-14 16:40:16 +0200

    Here we go again

--- lua/sftp/commands.lua
+++ lua/sftp/commands.lua
@@ -29,21 +29,42 @@ function M.diff_remote_file(args)
     return
   end
 
-  local temp_file = vim.fn.tempname()
+  -- Prepare paths
+  local downloaded_temp_file = vim.fn.tempname()
   local relative_file = string.sub(local_file, #server_config.local_path + 2)
   relative_file = string.gsub(relative_file, "\\", "/")
   local remote_file = server_config.remote_path .. "/" .. relative_file
 
+  -- Create a batch file for sftp
+  local batch_temp_file = vim.fn.tempname()
+  -- sftp's 'get' command might prefer forward slashes for the local path too
+  local normalized_download_path = string.gsub(downloaded_temp_file, "\\", "/")
+  local batch_content = string.format('get "%s" "%s"', remote_file, normalized_download_path)
+
+  local f, err = io.open(batch_temp_file, "w")
+  if not f then
+    log.error("Failed to create sftp batch file: " .. tostring(err))
+    return
+  end
+  f:write(batch_content)
+  f:close()
+
+  -- Construct the sftp command
   local sftp_command
+  local sftp_target
   if server_config.target then
-    sftp_command = string.format("sftp %s:%s %s", server_config.target, remote_file, temp_file)
+    sftp_target = server_config.target
   elseif server_config.host and server_config.user then
-    sftp_command = string.format("sftp %s@%s:%s %s", server_config.user, server_config.host, remote_file, temp_file)
+    sftp_target = string.format("%s@%s", server_config.user, server_config.host)
   else
     log.error("Invalid SFTP configuration for alias '" .. alias .. "'. Provide either 'target' or both 'host' and 'user'.")
+    os.remove(batch_temp_file) -- cleanup
     return
   end
 
+  sftp_command = string.format('sftp -b "%s" %s', batch_temp_file, sftp_target)
+
+  -- Execute the command
   local stderr_output = {}
   local job_id = vim.fn.jobstart(sftp_command, {
     on_stderr = function(_, data)
@@ -56,11 +77,14 @@ function M.diff_remote_file(args)
       end
     end,
     on_exit = function(_, exit_code)
+      -- Cleanup batch file first
+      os.remove(batch_temp_file)
+
       if exit_code == 0 then
         vim.schedule(function()
-          vim.cmd("diffsplit " .. temp_file)
+          vim.cmd("diffsplit " .. downloaded_temp_file)
           vim.defer_fn(function()
-            os.remove(temp_file)
+            os.remove(downloaded_temp_file)
           end, 1000)
         end)
       else
@@ -71,14 +95,19 @@ function M.diff_remote_file(args)
           error_message = error_message .. " Check your SFTP configuration and if the file exists on the remote server. Exit code: " .. tostring(exit_code)
         end
         log.error(error_message)
+        -- Also remove the (likely empty) downloaded file on error
+        os.remove(downloaded_temp_file)
       end
     end,
   })
 
   if job_id == 0 or job_id == -1 then
     log.error("Failed to start SFTP download job. Command: " .. sftp_command)
+    -- Cleanup batch file
+    os.remove(batch_temp_file)
   end
 end
+
 
 function M.init_config()
   local sftp_dir = vim.fn.getcwd() .. "/.sftp"
